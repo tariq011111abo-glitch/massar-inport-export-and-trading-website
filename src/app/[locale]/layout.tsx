3a823cf11cec39f2baeca4ab17ed93864b596099
tariq011111abo-glitch <tariq011111abo@gmail.com>
2026-09-05 08:13:39 -0700

    Commit 1 file to main

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 import { isLocale } from "@/lib/types";
 
 export function generateStaticParams() {
@@ -15,5 +16,23 @@ export default async function LocaleLayout({
 }) {
   const { locale } = await params;
   if (!isLocale(locale)) notFound();
-  return children;
+
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-WMECMKNP4N"
+
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WMECMKNP4N');
+        `}
+      </Script>
+      {children}
+    </>
+  );
 }
